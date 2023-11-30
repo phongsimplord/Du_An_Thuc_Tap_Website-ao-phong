ca1a075a3470244aead885fe9fa85dec20b4df55
Gia Phong <phong9ngontay@gmail.com>
2023-12-01 03:54:04 +0700

    a
    b

--- Front-end/src/routes/permission.js
+++
@@ -1,7 +0,0 @@
-// permission.js
-const userPermissions = {
-  NV: ['TrangChu', 'SanPham', 'Detail', 'GioHang', 'CheckOut', 'CheckOutQuick', 'ThankYou', 'UserAccount', 'DiaChi', 'History'],
-  KH: ['TrangChu', 'SanPham', 'Detail', 'GioHang', 'UserAccount', 'DiaChi', 'History']
-};
-
-export default userPermissions;
--- Front-end/src/services/ForgotPassword.js
+++
@@ -1,5 +0,0 @@
-import axios from '../custommize-axios';
-const forgotPasswordKH = (email) => {
-  return axios.post('/api/forgot-password?email=' + email);
-};
-export { forgotPasswordKH };
